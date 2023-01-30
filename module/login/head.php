8b290661ac3859060db80a6eefe1c646aa587779
whynot1217 <86461360+whynot1217@users.noreply.github.com>
2023-01-30 20:51:17 +0900

    Update head.php

--- module/login/head.php
+++ module/login/head.php
@@ -1,24 +1,7 @@
-<?
-session_cache_limiter('');
-session_start();
-Header("p3p: CP=\"CAO DSP AND SO ON\" policyref=\"/w3c/p3p.xml\"");
-
-//글로벌 변수 설정
-$GBL_USERID	= strtolower($_SESSION['ses_member_id']);
-$GBL_NAME	= $_SESSION['ses_member_name'];
-$GBL_MTYPE = $_SESSION['ses_member_type'];
-$GBL_PASSWORD = $_SESSION['ses_member_pwd'];
-
-$SYSTEM_DATE = date('Y-m-d');
-
-$strRoot = '../';
-$boardRoot = '../board/';
-
-?>
 
 <!doctype html>
 	<html lang="ko">
-		<head>
+		<head>https://github.com/whynot1217/hanpass/blob/master/module/login/head.php
 
 
 			<?
